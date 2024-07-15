897f3f422948216ef426a1e179ded10655a62c0d
BracketHive <info@brackethive.com>
2024-07-15 14:51:48 +0300

    feat: installed fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,16 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
+
   {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" }
   },
+
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' }
   },
+
+  { 'tpope/vim-fugitive' }
 }
